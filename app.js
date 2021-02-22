8975c6261fecebff11455acea4cc7409fb4e7132
jeewonn <jeewonn0504@gmail.com>
2021-02-22 14:58:56 +0900

    user paddle is moving now

--- app.js
+++ app.js
@@ -2,75 +2,112 @@
 const canvas = document.getElementById("pong");
 const ctx = canvas.getContext("2d");
 
-//main canvas
-function render() {
-  ctx.fillStyle = "#000";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
+const ball = {
+  x: canvas.width / 2, //300
+  y: canvas.height / 2, //200
+  radius: 10,
+  velocityX: 5, //속도  velocity = speed + direction
+  velocityY: 5,
+  speed: 7,
+  color: "WHITE",
+};
 
-  //paddle
-  const drawpaddle = (x, y, width, height) => {
-    ctx.fillStyle = "yellow";
-    ctx.fillRect(x, y, width, height);
-  };
+const user = {
+  x: 0,
+  y: (canvas.height - 100) / 2,
+  width: 10,
+  height: 100,
+  score: 0,
+  color: "WHITE",
+};
 
-  drawpaddle(0, 0, 20, 100); //user
-  drawpaddle(580, 0, 20, 100); //computer
+const com = {
+  x: canvas.width - 10,
+  y: (canvas.height - 100) / 2,
+  width: 10,
+  height: 100,
+  score: 0,
+  color: "WHITE",
+};
 
-  //net (나중에 다시 점선 만들기)
-  function drawnet(x, y, width, height) {
-    ctx.fillStyle = "white";
-    ctx.fillRect(x, y, width, height);
-  }
+const net = {
+  x: (canvas.width - 2) / 2,
+  y: 0,
+  height: 10,
+  width: 2,
+  color: "WHITE",
+};
 
-  drawnet(300, 0, 3, 400);
-
-  // score text
-  function drawscore(text, w, h) {
-    ctx.font = "30px Comic Sans MS";
-    ctx.fillStyle = "pink";
-    ctx.textAlign = "center";
-    ctx.fillText(text, w, h);
-  }
-
-  drawscore("score", canvas.width / 4, 30);
-  drawscore("score", (3 * canvas.width) / 4, 30);
-
-  // ball
-  function drawball(x, y, r, color) {
-    ctx.fillStyle = color;
-    ctx.beginPath();
-    ctx.arc(x, y, r, 0, Math.PI * 2, false);
-    ctx.closePath();
-    ctx.fill();
-  }
-
-  drawball(300, 200, 15, "green");
+// 네모 그리기
+function drawRect(x, y, w, h, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, w, h);
 }
 
-render();
-// Movement
-function gameLoop() {
+//원 그리기
+function drawArc(x, y, r, color) {
+  ctx.fillStyle = color;
+  ctx.beginPath();
+  ctx.arc(x, y, r, 0, Math.PI * 2, true);
+  ctx.closePath();
+  ctx.fill();
+}
+
+// 글자 쓰기
+function drawText(text, x, y) {
+  ctx.fillStyle = "#FFF";
+  ctx.font = "75px arial";
+  ctx.fillText(text, x, y);
+}
+
+//moving paddle
+canvas.addEventListener("mousemove", getMousePos);
+
+function getMousePos(evt) {
+  let rect = canvas.getBoundingClientRect();
+
+  user.y = evt.clientY - rect.top - user.height / 2;
+  console.log(evt.clientY);
+}
+
+//ball reset
+
+function update() {
+  //moving ball
+  ball.x += ball.velocityX; // 즉,X+
+  ball.y += ball.velocityY; // Y+
+}
+
+// all draw
+function render() {
+  // clear the canvas
+  drawRect(0, 0, canvas.width, canvas.height, "#000");
+
+  // draw the user score to the left
+  drawText(user.score, canvas.width / 4, canvas.height / 5);
+
+  // draw the COM score to the right
+  drawText(com.score, (3 * canvas.width) / 4, canvas.height / 5);
+
+  // draw the net
+  drawRect(300, 0, 3, 400);
+  // draw the user's paddle
+  drawRect(user.x, user.y, user.width, user.height, user.color);
+
+  // draw the COM's paddle
+  drawRect(com.x, com.y, com.width, com.height, com.color);
+
+  // draw the ball
+  drawArc(ball.x, ball.y, ball.radius, ball.color);
+
+  canvas.addEventListener("mousemove", getMousePos);
+}
+
+// 최종 실행
+function game() {
   update();
   render();
 }
 
-setInterval(gameLoop, 100 / 60); //1분당 60번 함수 실행(즉 1초당 실행함)
-
-// 공 움직이기, 공이 벽을 쳤는지 또는 패들을 쳤는지 확인, 패들 움직이기, 컴퓨터 패들 움직이기, collision detection on paddles
-const ball = {
-  x: 300,
-  y: 200,
-  velocityX: 5,
-  velocityY: 5, //speed + direction
-  speed: 5,
-};
-
-const paddle = {};
-
-function update() {
-  //패들 움직이기
-
-  // move ball
-  ball.x += ball.velocityX;
-  ball.y += ball.velocityY;
-}
+//call the game function 50 times every 1 Sec
+setInterval(game, 1000 / 50);
