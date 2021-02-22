8b3ef1fb4734be08e28a958ce513ed2003cedc33
jeewonn <jeewonn0504@gmail.com>
2021-02-22 19:27:38 +0900

    all done probably...

--- app.js
+++ app.js
@@ -71,16 +71,78 @@ function getMousePos(evt) {
 }
 
 //ball reset
+function ballReset() {
+  ball.x = canvas.width / 2;
+  ball.y = canvas.height / 2;
+  ball.speed = 7;
+  ball.velocityX = -ball.velocityX; //공을 반대쪽으로 보냄
+}
+
+// 공이 패들에 닿을때
+function collision(b, p) {
+  p.top = p.y;
+  p.bottom = p.y + p.height;
+  p.left = p.x;
+  p.right = p.x + p.width;
+
+  b.top = b.y - b.radius;
+  b.bottom = b.y + b.radius;
+  b.left = b.x - b.radius;
+  b.right = b.x + b.radius;
+
+  return (
+    b.right > p.left && b.bottom > p.top && b.left < p.right && b.top < p.bottom
+  );
+}
 
 function update() {
-  //moving ball
-  ball.x += ball.velocityX; // 즉,X+
+  // 점수
+  if (ball.x - ball.radius < 0) {
+    com.score++;
+    ballReset();
+  } else if (ball.x + ball.radius > canvas.width) {
+    user.score++;
+    ballReset();
+  }
+
+  //com paddle
+  com.y += (ball.y - (com.y + com.height / 2)) * 0.1;
+
+  //moving ball (처음)
+  ball.x += ball.velocityX; // 즉,X+ //이것만 실행 하면 오른쪽 수직방향으로 공이 쭉 움직인다.. 왜 쭉 갈까..?
   ball.y += ball.velocityY; // Y+
+
+  // 공이 bottom, top of canvas 칠때
+  if (ball.y + ball.radius >= canvas.height || ball.y - ball.radius <= 0) {
+    //ball.y + ball.radius 값은 bottom of ball 임.
+    ball.velocityY = -ball.velocityY;
+  } //공의 top 과 bottom 이 캔버스의 bottom과 top 에 부딫힐 경우 velocity 의 값이 reverse 되어야 함.
+
+  // paddle이 user 패들인지 com 인지 확인
+  let player = ball.x + ball.radius < canvas.width / 2 ? user : com;
+
+  // 공이 패들 칠때
+  if (collision(ball, player)) {
+    // ball.y랑 paddel의 y 값이 똑같아진다. (즉, 볼이 패들을 치는 위치)
+    let collidePoint = ball.y - (player.y + player.height / 2);
+
+    //값을 -1,0,1로 만들어줌
+    collidePoint = collidePoint / (player.y + player.height / 2);
+
+    //튕겨나가는 위치 알기 위해
+    let angle = (Math.PI / 4) * collidePoint;
+
+    let direction = ball.x + ball.radius < canvas.width / 2 ? 1 : -1;
+    ball.velocityX = Math.cos(angle) * ball.speed * direction;
+    ball.velocityY = Math.sin(angle) * ball.speed;
+
+    ball.speed += 0.8;
+  }
 }
 
 // all draw
 function render() {
-  // clear the canvas
+  //  canvas
   drawRect(0, 0, canvas.width, canvas.height, "#000");
 
   // draw the user score to the left
@@ -91,6 +153,7 @@ function render() {
 
   // draw the net
   drawRect(300, 0, 3, 400);
+
   // draw the user's paddle
   drawRect(user.x, user.y, user.width, user.height, user.color);
 
@@ -99,8 +162,6 @@ function render() {
 
   // draw the ball
   drawArc(ball.x, ball.y, ball.radius, ball.color);
-
-  canvas.addEventListener("mousemove", getMousePos);
 }
 
 // 최종 실행
